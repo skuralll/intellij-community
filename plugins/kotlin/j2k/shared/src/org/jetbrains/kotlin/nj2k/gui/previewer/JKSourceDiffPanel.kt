1d2297e18b239d6788c38bbb27bddd958ee3c268
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-11-11 18:40:19 +0900

    Add JKSourceDiffPanel to Previewer for displaying source code differences.

---
+++ plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/previewer/JKSourceDiffPanel.kt
@@ -0,0 +1,28 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.kotlin.nj2k.gui.previewer
+
+import com.intellij.openapi.fileTypes.FileTypeManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.Splitter
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.components.JBPanel
+import org.jetbrains.kotlin.nj2k.gui.common.SourceViewField
+import java.awt.BorderLayout
+
+class JKSourceDiffPanel(project: Project, rootFile: VirtualFile) : JBPanel<JBPanel<*>>(BorderLayout()) {
+
+    // ソースビューア
+    private val javaViewer = SourceViewField(null, project, FileTypeManager.getInstance().getFileTypeByExtension("java"), true)
+    private val ktViewer = SourceViewField(null, project, FileTypeManager.getInstance().getFileTypeByExtension("kt"), true)
+
+    init {
+        // Splitterを使って横に分割表示
+        val splitter = Splitter(false, 0.5f) // trueは水平方向に分割、0.5fは分割位置（中央）
+        splitter.dividerWidth = 2
+        splitter.setResizeEnabled(false)
+        splitter.firstComponent = javaViewer
+        splitter.secondComponent = ktViewer
+        add(splitter, BorderLayout.CENTER)
+    }
+
+}
--- plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/previewer/Previewer.kt
+++ plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/previewer/Previewer.kt
@@ -1,8 +1,10 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.nj2k.gui.previewer
 
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.dsl.builder.Align
 import com.intellij.ui.dsl.builder.panel
 import org.jetbrains.kotlin.idea.base.resources.KotlinBundle
 import org.jetbrains.kotlin.nj2k.gui.common.JKFileTreePanel
@@ -10,22 +12,28 @@ import java.awt.Dimension
 import javax.swing.JComponent
 
 // 変換プレビュー
-class Previewer(private val rootFile: VirtualFile) : DialogWrapper(true) {
+class Previewer(private val project: Project, private val rootFile: VirtualFile) : DialogWrapper(true) {
+
+    private val diffView : JKSourceDiffPanel
 
     init {
         title = KotlinBundle.message("action.j2k.gui.title")
+        diffView = JKSourceDiffPanel(project, rootFile)
         init()
     }
 
     override fun createCenterPanel(): JComponent {
         // ファイルエクスプローラ
         val fileExplorer = JKFileTreePanel(rootFile)
-        fileExplorer.preferredSize = Dimension(400, 400)
+        fileExplorer.preferredSize = Dimension(400, 600)
+        // Diff
+        diffView.preferredSize = Dimension(800, 600)
         // パネル作成
         return panel {
             row {
-                cell(fileExplorer)
-            }
+                cell(fileExplorer).align(Align.FILL).resizableColumn()
+                cell(diffView).align(Align.FILL)
+            }.resizableRow()
         }
     }
 }
