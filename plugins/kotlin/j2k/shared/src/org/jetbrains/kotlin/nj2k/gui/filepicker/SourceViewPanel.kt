492b19df2a6cc4858cc2f80c35bf760230eac378
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-11-14 18:31:51 +0900

    Implement method to get labeled SourceViewPanel

--- plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/filepicker/SourceViewPanel.kt
+++ plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/filepicker/SourceViewPanel.kt
@@ -6,25 +6,41 @@ import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.findDocument
+import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
+import com.intellij.ui.dsl.builder.Align
+import com.intellij.ui.dsl.builder.panel
 import org.jetbrains.kotlin.nj2k.gui.common.SourceViewField
 import java.awt.BorderLayout
+import javax.swing.JComponent
 
 class SourceViewPanel(project: Project, document: Document?, fileType: FileType) : JBPanel<JBPanel<*>>(BorderLayout()) {
 
     // VirtualFileから作成するためのセカンダリコンストラクタ
     constructor(project: Project, file: VirtualFile) : this(project, file.findDocument(), file.fileType)
 
+    // ラベル
+    val label = JBLabel(" ")
     // エディタ
     private val sourceViewer: SourceViewField = SourceViewField(document, project, fileType, true)
 
     init {
-        // リサイズさせるためにCENTERに配置
+        // ラベル
+        //add(JBLabel("This is test"), BorderLayout.NORTH)
+        // コードビューア, リサイズさせるためにCENTERに配置
         add(sourceViewer, BorderLayout.CENTER)
     }
 
     // ファイル切り替えメソッド
     fun switchFile(file: VirtualFile) {
         sourceViewer.switchFile(file)
+    }
+
+    // ラベル付きのパネルを取得
+    fun getLabeledPanel() : JComponent {
+        return panel {
+            row { label }
+            row { cell(this@SourceViewPanel).align(Align.FILL) }
+        }
     }
 }
