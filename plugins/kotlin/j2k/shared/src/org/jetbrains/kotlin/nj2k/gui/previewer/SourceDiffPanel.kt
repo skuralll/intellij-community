8e39496ea224bf70163e74ca58ed24ca370682df
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-11-15 14:18:48 +0900

    Add method to retrieve file icon based on file type in SourceDiffPanel class.

--- plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/previewer/SourceDiffPanel.kt
+++ plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/gui/previewer/SourceDiffPanel.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.kotlin.nj2k.gui.previewer
 
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeManager
@@ -10,6 +11,7 @@ import com.intellij.ui.components.JBPanel
 import org.jetbrains.kotlin.nj2k.gui.filepicker.SourceViewPanel
 import java.awt.Dimension
 import java.awt.GridLayout
+import javax.swing.Icon
 
 class SourceDiffPanel(project: Project, rootFile: VirtualFile) : JBPanel<JBPanel<*>>(GridLayout(1, 2)) {
 
@@ -35,15 +37,24 @@ class SourceDiffPanel(project: Project, rootFile: VirtualFile) : JBPanel<JBPanel
     }
 
     // 変換前情報をセット
-    fun setBefore(document: Document?, fileType: FileType){
+    fun setBefore(document: Document?, fileType: FileType) {
         beforeViewer.switchFile(document, fileType)
         beforeViewer.label.icon = fileType.icon
+        beforeViewer.label.icon = getFileIcon(document, fileType)
     }
 
     // 変換後情報をセット
-    fun setAfter(document: Document?, fileType: FileType){
+    fun setAfter(document: Document?, fileType: FileType) {
         afterViewer.switchFile(document, fileType)
-        afterViewer.label.icon = fileType.icon
+        afterViewer.label.icon = getFileIcon(document, fileType)
+    }
+
+    // ファイルに適したアイコンを取得するメソッド
+    private fun getFileIcon(document: Document?, fileType: FileType): Icon? {
+        return when{
+            fileType.icon != AllIcons.FileTypes.Unknown -> fileType.icon
+            else -> AllIcons.FileTypes.Any_type
+        }
     }
 
 }
