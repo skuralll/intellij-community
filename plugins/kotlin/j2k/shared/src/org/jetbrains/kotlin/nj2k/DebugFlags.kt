f4277aa127c93dde90f8ed7b891356037829c540
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-12-12 13:38:48 +0900

    Set `doDeleteLabel` flag to true in DebugFlags

--- plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/DebugFlags.kt
+++ plugins/kotlin/j2k/shared/src/org/jetbrains/kotlin/nj2k/DebugFlags.kt
@@ -3,7 +3,7 @@ package org.jetbrains.kotlin.nj2k
 
 object DebugFlags {
     // ラベルを削除するかどうか
-    const val doDeleteLabel: Boolean = false
+    const val doDeleteLabel: Boolean = true
     // 全てのラベル情報を表示するかどうか
     const val showLabelsInfo = false
 }
